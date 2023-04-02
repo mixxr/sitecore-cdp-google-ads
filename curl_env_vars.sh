881b3eb67afc61b881acab39651943cecf21b4b9
Andres Aya <andresayac@gmail.com>
2023-04-01 20:24:58 -0500

    Add conversion offline mode

--- curl_env_vars.sh
+++ curl_env_vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export API_VERSION="11";
+export API_VERSION="13";
 export CLIENT_ID="< from the json >";
 export CLIENT_SECRET="< from the json >";
 export DEVELOPER_TOKEN="< fro mthe UI >";
@@ -7,3 +7,4 @@ export OAUTH2_ACCESS_TOKEN="< from the refresh token call >";
 export MANAGER_CUSTOMER_ID="<from the UI, left corner >";
 export CUSTOMER_ID="<from the UI, id on the right>";
 export REFRESH_TOKEN="< upon authentication >"
+export ACTION_CONVERSION_ID="< from the UI >";
